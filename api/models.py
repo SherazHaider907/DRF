2f6db7d63ed9d0f2f32aee078b1622637103d4af
SherazHaider907 <Sherazhaider665@gmail.com>
2026-02-12 16:09:36 +0500

    make Product model

--- api/models.py
+++ api/models.py
@@ -1,2 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
+class Product(models.Model):
+    name= models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/',blank=True,null=True)
+
+    @property
+    def in_stock(self):
+        return self.stock > 0
+    
+    def __str__(self):
+        return self.name
