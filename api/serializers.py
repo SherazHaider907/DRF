97a9bcd8bc360068485f5b17b09ce873b0d00032
SherazHaider907 <Sherazhaider665@gmail.com>
2026-02-14 12:08:12 +0500

    add serializerMethod field

--- api/serializers.py
+++ api/serializers.py
@@ -23,16 +23,23 @@ class ProductSerializer(serializers.ModelSerializer):
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
-        fields = ('product','quantity',) 
+        fields = ('product','quantity') 
 
 class OrderSerializer(serializers.ModelSerializer):
-    items = OrderItemSerializer(many=True,read_only=True)
+    items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField(method_name='total')
+
+    def total(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+
     class Meta:
         model = Order
         fields = (
-        'order_id',
-        'user',
-        'created_at',
-        'status',
-        'items'
+            'order_id',
+            'user',
+            'created_at',
+            'status',
+            'items',
+            'total_price',
         )
